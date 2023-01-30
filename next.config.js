6e53f957b8356fc76cf654d5ce2db2449977a8c5
Inam Ur Rahman <mr.inamurrahman@gmail.com>
2023-01-30 23:26:10 +0500

    fresh

--- next.config.js
+++ next.config.js
@@ -1,16 +1,13 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   experimental: {
-//     appDir: true,
-//   },
-// }
-
-
-// module.exports = nextConfig
-module.exports =  {
-  experimental: {
-    externalDir: true
-  },
-  // Potential new config flag:
-  disableExperimentalFeaturesWarning: true
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
 }
+module.exports = nextConfig
+module.exports = {
+  compress: true,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+};
