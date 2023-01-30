6b3371c20bb44b88150ba8dfa416fc5ddfc5a149
Inam Ur Rahman <mr.inamurrahman@gmail.com>
2023-01-30 23:54:26 +0500

    fresh

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
-module.exports = nextConfig
-module.exports = {
-  compress: true,
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
+  pageExtensions: ['ts', 'tsx', 'jsx'],
+  experimental: {
+    appDir: true,
+    mdxRs: true,
+  }
 };
+
+module.exports = (nextConfig);
